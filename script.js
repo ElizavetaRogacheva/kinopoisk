fea7cbad985507086a623a0d27085fd348b81ee4
Елизавета Рогачева <liza-97-97-97@mail.ru>
2019-06-19 20:25:14 +0500

    link API server

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function searchApi(evt) {
     evt.preventDefault();
     console.log('fgfg');
     const searchText = document.querySelector('.form-control').value,
-    server = 'https://www.themoviedb.org/'
+    server = 'https://api.themoviedb.org/3/search/multi?api_key=a099fd17815f4a790c4f1a6f06a8e34c&language=ru&query=---'
     console.log(requestApi(server));
 }
 
